161dc1d75c3fcd89ee2a08b18a7d44778443f456
td-diana <99271477+td-diana@users.noreply.github.com>
2022-10-25 08:38:22 +0300

    helmet, limit

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const router = require('./routes/routes');
 const { handleError } = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 const app = express();
 app.use(cors());
-
+app.use(limiter);
 app.use(bodyParser.json());
 
 app.use(requestLogger); // подключаем логгер запросов
 app.use(router);
+app.use(helmet());
 app.use(errorLogger); // подключаем логгер ошибок
 
 app.use(errors());
